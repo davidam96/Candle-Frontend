5a4021d5d03e0da2f1c19f6dd4459f5b50f4af71
David Arroyo Moreno <david.arroyo-ext@adentia.es>
2022-04-29 02:12:47 +0200

    v0.3

--- app/src/main/java/net/davidam/candle/fragments/SearchFragment.kt
+++ app/src/main/java/net/davidam/candle/fragments/SearchFragment.kt
@@ -1,24 +1,29 @@
 package net.davidam.candle.fragments
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
+import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.fragment.app.Fragment
 import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.functions.FirebaseFunctions
 import com.google.firebase.functions.ktx.functions
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.ktx.app
 import com.google.gson.Gson
-import net.davidam.candle.MainActivity
 import net.davidam.candle.R
 import net.davidam.candle.model.Word
 import net.davidam.candle.model.WordRequest
 import net.davidam.candle.model.WordResponse
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -30,6 +35,9 @@ private const val ARG_PARAM2 = "param2"
  * Use the [SearchFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+@Suppress("UNCHECKED_CAST")
+@RequiresApi(Build.VERSION_CODES.N)
 class SearchFragment : Fragment(), SearchView.OnQueryTextListener {
 
     // TODO: Rename and change types of parameters
@@ -58,7 +66,6 @@ class SearchFragment : Fragment(), SearchView.OnQueryTextListener {
                     putString(ARG_PARAM2, param2)
                 }
             }
-        const val TAG = "dabudin"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -103,7 +110,7 @@ class SearchFragment : Fragment(), SearchView.OnQueryTextListener {
     override fun onQueryTextSubmit(text: String): Boolean {
         //Activate Firestore machinery here to create a new document if the queried
         //word doesn't exist, or return a document with the word info if it does.
-        checkWords(text)
+        errorHandler(text)
         return false
     }
     // *************** SEARCH BAR ***************
@@ -113,24 +120,29 @@ class SearchFragment : Fragment(), SearchView.OnQueryTextListener {
     // ********* GOOGLE CLOUD FUNCTIONS *********
     //docs: (https://firebase.google.com/docs/functions/callable)
 
-    private fun checkWords(text: String) {
+    private fun errorHandler(text: String) {
 
         // Handling errors
         // (limit the user input to max 10 words and 100 chars to
         // prevent fraudulent use of this function by the client).
         if (text.length > 100) {
-            Toast.makeText(activity,
-                "ERROR: El limite maximo son 100 caracteres",
-                Toast.LENGTH_LONG).show()
+            errorSnack("El limite maximo son 100 caracteres")
         }
         else if (text.split("\\s".toRegex()).size >= 10) {
-            Toast.makeText(activity,
-                "ERROR: El limite maximo son 10 palabras",
-                Toast.LENGTH_LONG).show()
+            errorSnack("El limite maximo son 10 palabras")
         }
         else {
-            //Serializing the input words provided by the SearchBar
-             dictionaryGenerator(text)
+            //Let's make a toast with the response given by GCF's dictionaryGenerator()
+            dictionaryGenerator(text).continueWith { task ->
+                val response = task.result!!
+                if (response.errorCode != -1) {
+                    //toast("ERROR: ${response.error}")
+                    errorSnack(response.error)
+                }
+                else {
+                    toast(": ${Gson().toJson(response)}")
+                }
+            }
         }
     }
 
@@ -139,27 +151,45 @@ class SearchFragment : Fragment(), SearchView.OnQueryTextListener {
         val data = Gson().toJson(WordRequest(text))
 
         return functions
-            .getHttpsCallable("dictionaryGenerator-2")
+            .getHttpsCallable("dictionaryGenerator")
             .call(data)
             .continueWith { task ->
                 // This continuation runs on either success or failure, but if the task
                 // has failed then result will throw an Exception which will be
                 // propagated down.
                 val result = task.result?.data as HashMap<*, *>
+                val contents = result["contents"] as HashMap<*, *>
                 val word = Word("",
-                    result["words"] as String,
-                    result["wordCount"] as Int,
-                    result["combinations"] as MutableList<String>?,
-                    result["types"] as MutableList<String>?,
-                    result["meanings"] as MutableList<String>?,
-                    result["translations"] as MutableList<String>?,
-                    result["synonyms"] as MutableList<String>?,
-                    result["examples"] as MutableList<String>?)
-                Log.d(TAG, Gson().toJson(word))
+                    contents["words"] as String,
+                    contents["wordCount"] as Int,
+                    contents["types"] as MutableList<String>?,
+                    contents["meanings"] as MutableList<String>?,
+                    contents["translations"] as MutableList<String>?,
+                    contents["synonyms"] as MutableList<String>?,
+                    contents["examples"] as MutableList<String>?,
+                    contents["combinations"] as MutableList<String>,)
                 val response = WordResponse(word, result["error"] as String, result["errorCode"] as Int)
-                Log.d(TAG, Gson().toJson(response))
                 response
             }
     }
     // ********* GOOGLE CLOUD FUNCTIONS *********
+
+
+
+    // ************ OTHER FUNCTIONS *************
+    private fun toast(text: String): Toast {
+        return Toast.makeText(activity, text, Toast.LENGTH_SHORT)
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun errorSnack(text: String) {
+        //We make a personalised snackbar with the background color as red
+        val styledText = Html.fromHtml("<b>ERROR:</b> $text", Build.VERSION.SDK_INT)
+        val snackBar = Snackbar.make(requireView(), styledText, Snackbar.LENGTH_LONG)
+        snackBar.view.background = resources.getDrawable(R.drawable.snackbar_error, null)
+        snackBar.view.findViewById<TextView>(com.google.android.material.R.id.snackbar_text).maxLines = 2
+        snackBar.setTextColor(Color.WHITE)
+        snackBar.show()
+    }
+    // ************ OTHER FUNCTIONS *************
 }
--- app/src/main/java/net/davidam/candle/model/Classes.kt
+++ app/src/main/java/net/davidam/candle/model/Classes.kt
@@ -2,20 +2,20 @@ package net.davidam.candle.model
 
 import java.io.Serializable
 
-data class WordRequest(var words: String=""): Serializable
-
 data class Word(var id: String = "",
                 var words: String = "",
                 var wordCount: Int = 0,
-                var combinations: MutableList<String>? = null,
                 var types: MutableList<String>? = null,
                 var meanings: MutableList<String>? = null,
                 var translations: MutableList<String>? = null,
                 var synonyms: MutableList<String>? = null,
-                var examples: MutableList<String>? = null
-)
+                var examples: MutableList<String>? = null,
+                var combinations: MutableList<String>? = null
+): Serializable
 
-data class WordResponse(var data: Word? = null,
+data class WordRequest(var words: String=""): Serializable
+
+data class WordResponse(var contents: Word? = null,
                         var error: String = "",
                         var errorCode: Int = -1
-)
+): Serializable
